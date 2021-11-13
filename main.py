39895127228ccc089bf9f83b28e0cb0a081a78e8
Nguyen Pham <nguyen.gntvn@gmail.com>
2021-11-13 22:48:58 +0700

    update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_caching import Cache
 from uwsgidecorators import postfork
 from con import Controller
 import sen
-import chart
 
 con = Controller()
 
@@ -16,7 +15,6 @@ cache.init_app(app)
 @postfork
 def init():
     sen.fetch()
-    chart.gen_chart()
 
 
 @cache.cached(timeout=300)
