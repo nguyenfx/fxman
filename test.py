39c5a1630c0bcb6b77aba06e1ec4d09af52830f4
Nguyen Pham <nguyen.gntvn@gmail.com>
2021-11-16 09:05:18 +0700

    update

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ from con import Controller
 import sen
 
 if __name__ == "__main__":
-    sen.fetch()
-    chart.gen_chart()
+    #sen.fetch()
+    #chart.gen_chart()
     con = Controller()
     signal = con.get_signal("CADJPY")
     print(signal)
-    sentiments = con.get_sentiments()
-    print(sentiments)
-    status = con.get_status()
-    print(status)
+    #sentiments = con.get_sentiments()
+    #print(sentiments)
+    #status = con.get_status()
+    #print(status)
     con.calculate_all_statistic()
     accounts = con.get_accounts()
     for account in accounts:
