94737053e553cadcb45cd1a4773bb83837921c12
Nguyen Pham <nguyen.gntvn@gmail.com>
2021-11-18 19:21:07 +0700

    update

--- ta.py
+++ ta.py
@@ -1,12 +1,14 @@
 import json
 
-from tradingview_ta import TA_Handler, Interval
+from tradingview_ta import *
 
 from con import Controller
 
-Forex = {"EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCHF", "USDJPY", "USDCAD", "EURJPY", "GBPJPY", "AUDJPY", "NZDJPY",
-         "CHFJPY", "CADJPY", "EURGBP", "EURAUD", "EURNZD", "EURCHF", "EURCAD", "GBPAUD", "GBPNZD", "GBPCHF", "GBPCAD",
-         "AUDNZD", "AUDCHF", "AUDCAD", "NZDCHF", "NZDCAD", "CADCHF"}
+Symbols = ["FX_IDC:EURUSD", "FX_IDC:GBPUSD", "FX_IDC:AUDUSD", "FX_IDC:NZDUSD", "FX_IDC:USDCHF", "FX_IDC:USDJPY",
+           "FX_IDC:USDCAD", "FX_IDC:EURJPY", "FX_IDC:GBPJPY", "FX_IDC:AUDJPY", "FX_IDC:NZDJPY", "FX_IDC:CHFJPY",
+           "FX_IDC:CADJPY", "FX_IDC:EURGBP", "FX_IDC:EURAUD", "FX_IDC:EURNZD", "FX_IDC:EURCHF", "FX_IDC:EURCAD",
+           "FX_IDC:GBPAUD", "FX_IDC:GBPNZD", "FX_IDC:GBPCHF", "FX_IDC:GBPCAD", "FX_IDC:AUDNZD", "FX_IDC:AUDCHF",
+           "FX_IDC:AUDCAD", "FX_IDC:NZDCHF", "FX_IDC:NZDCAD", "FX_IDC:CADCHF"]
 
 Intervals = {Interval.INTERVAL_1_DAY, Interval.INTERVAL_4_HOURS, Interval.INTERVAL_1_HOUR, Interval.INTERVAL_15_MINUTES}
 
@@ -56,17 +58,11 @@ def upsert(symbol, timeframe, analysis):
 
 
 def fetch():
-    for symbol in Forex:
-        for interval in Intervals:
-            handle = TA_Handler(
-                symbol=symbol,
-                screener="forex",
-                exchange="FX_IDC",
-                interval=interval
-            )
-            analysis = handle.get_analysis()
-            upsert(symbol, interval, analysis)
     for interval in Intervals:
+        data = get_multiple_analysis(screener="forex", interval=interval, symbols=Symbols)
+        for symbol in Symbols:
+            analysis = data[symbol]
+            upsert(analysis.symbol, interval, analysis)
         handle = TA_Handler(
             symbol="GOLD",
             screener="cfd",
@@ -75,7 +71,6 @@ def fetch():
         )
         analysis = handle.get_analysis()
         upsert("XAUUSD", interval, analysis)
-    for interval in Intervals:
         handle = TA_Handler(
             symbol="BTCUSDT",
             screener="crypto",
