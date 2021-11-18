f612696990a9bba36d5bd8ac24a62dbfdc7c8266
Nguyen Pham <nguyen.gntvn@gmail.com>
2021-11-18 12:59:47 +0700

    update

--- con.py
+++ con.py
@@ -184,7 +184,10 @@ class Controller:
     def get_tas(self):
         db = self.get_db()
         cursor = db.cursor()
-        statement = "SELECT * FROM tas ORDER BY symbol, interval "
+        statement = "select distinct tas.symbol, sub1.ma, sub2.os from tas inner join (select symbol, sum(ma) as ma " \
+                    "from tas where interval = '1d' or interval = '4h' group by symbol) as sub1 on tas.symbol = " \
+                    "sub1.symbol inner join (select symbol, sum(os) as os from tas where interval = '1h' or interval " \
+                    "= '15m' group by symbol) as sub2 on tas.symbol = sub2.symbol "
         cursor.execute(statement)
         return cursor.fetchall()
 
